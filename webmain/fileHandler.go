b835652d21f1e9fe8515c8f6db4c359bce9e213b
zhangshy <zaozh1990@163.com>
2015-08-11 20:18:08 +0800

    修改上传成功的提示

--- webmain/fileHandler.go
+++ webmain/fileHandler.go
@@ -9,7 +9,7 @@ import (
 
 func fileHandler(w http.ResponseWriter, r *http.Request) {
 	filenames, _ := fileutil.GetCurrentDirFiles("D:\\lqbz\\test\\go")
-	t, err := template.ParseFiles("template\\html\\browse.html")
+	t, err := template.ParseFiles("template/html/browse.html")
 	if err != nil {
 		log.Println(err)
 	}
--- webmain/upload.go
+++ webmain/upload.go
@@ -6,28 +6,29 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 )
 
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
+	t, err := template.ParseFiles("template/html/upload.html")
+	if err != nil {
+		log.Println(err)
+	}
 	if r.Method == "GET" {
-		t, err := template.ParseFiles("template\\html\\upload.html")
-		if err != nil {
-			log.Println(err)
-		}
 		t.Execute(w, nil)
 	} else {
 		file, handle, err := r.FormFile("file")
 		if err != nil {
 			log.Println(err)
 		}
-		f, err := os.OpenFile("files\\"+handle.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+		f, err := os.OpenFile(filepath.Join("files", handle.Filename), os.O_WRONLY|os.O_CREATE, 0666)
 		if err != nil {
 			log.Println(err)
 		}
 		defer f.Close()
 		defer file.Close()
 		io.Copy(f, file)
-		io.WriteString(w, "上传成功")
+		t.Execute(w, "上传成功")
 	}
 }
