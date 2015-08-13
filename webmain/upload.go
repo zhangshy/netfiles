2e1f14de4b31b505ca5e18fe3b22bc74949f826f
zhangshy <zaozh1990@163.com>
2015-08-13 20:19:27 +0800

    使用多个模板文件进行渲染

--- webmain/upload.go
+++ webmain/upload.go
@@ -9,14 +9,20 @@ import (
 	"path/filepath"
 )
 
+//struct中的各项首字母要大写，否则渲染时用"."获取不到
+type Info struct {
+	Title string
+	Ret   string
+}
+
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
-	t, err := template.ParseFiles("template/html/upload.html")
-	if err != nil {
-		log.Println(err)
-	}
+	tmpl := make(map[string]*template.Template)
+	//渲染多个模板文件
+	tmpl["upload"] = template.Must(template.ParseFiles("template/html/layout.html", "template/html/upload.html"))
+	info := Info{Title: "上传测试", Ret: ""}
 	if r.Method == "GET" {
-		t.Execute(w, nil)
+		tmpl["upload"].ExecuteTemplate(w, "layout", info)
 	} else {
 		file, handle, err := r.FormFile("file")
 		if err != nil {
@@ -29,6 +35,7 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 		defer f.Close()
 		defer file.Close()
 		io.Copy(f, file)
-		t.Execute(w, "上传成功")
+		info.Ret = "上传成功"
+		tmpl["upload"].ExecuteTemplate(w, "layout", info)
 	}
 }
