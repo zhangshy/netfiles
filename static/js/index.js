3b6a29c83d58d797151d9956b56a974f762ac322
zhangshy <zaozh1990@163.com>
2015-09-08 23:41:53 +0800

    使用state和界面交互

--- static/js/index.js
+++ static/js/index.js
@@ -1,26 +1,33 @@
-var NetFile = React.createClass({
+
+var BrowseFile = React.createClass({
   render: function() {
-    return (
-      <div className="netFile row">
-        <SideBar />
-        <MenuResponse />
+    return(
+      <div className="browseFile">
+        This is BrowseFile!
+      </div>
+    );
+  }
+});
+
+var UploadFile = React.createClass({
+  render:function() {
+    return(
+      <div className="uploadFile">
+        This is UploadFile!
       </div>
     );
   }
 });
 
 var SideBar = React.createClass({
-  browseFile: function() {
-    console.log("browseFile call");
-  },
-  uploadFile: function() {
-    console.log("uploadFile call")
+  handleChange: function(e) {
+    this.props.onInputClick(e);
   },
   render: function() {
     return (
       <div className="sideBar col-xs-3 col-md-1">
-        <input className="btn btn-default" type="button" value="文件浏览" onClick={this.browseFile} /><br/>
-        <input className="btn btn-default" type="button" value="文件上传" onClick={this.uploadFile}/>
+        <input className="btn btn-default" type="button" value="文件浏览" onClick={this.handleChange.bind(this, "BrowseFile")} /><br/>
+        <input className="btn btn-default" type="button" value="文件上传" onClick={this.handleChange.bind(this, "UploadFile")}/>
       </div>
     );
   }
@@ -28,13 +35,37 @@ var SideBar = React.createClass({
 
 var MenuResponse = React.createClass({
   render: function() {
+    var key = this.props.inputName;
+    var response = RESPONSES[key];
     return (
       <div className="menuResponse col-xs-9 col-md-6">
-        MenuResponse!!
+        {response}
       </div>
     );
   }
 });
+
+var NetFile = React.createClass({
+  handleInputClick: function(e) {
+    console.log("set state " + e);
+    this.setState({inputName: e});
+  },
+  getInitialState: function() {
+    return {
+      inputName: "BrowseFile"
+    };
+  },
+  render: function() {
+    return (
+      <div className="netFile row">
+        <SideBar onInputClick={this.handleInputClick} />
+        <MenuResponse inputName={this.state.inputName} />
+      </div>
+    );
+  }
+});
+
+var RESPONSES = {BrowseFile: <BrowseFile />, UploadFile: <UploadFile />};
 
 React.render(
   <NetFile />,
